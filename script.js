96b391d072900db5d7c7907934d180e548168901
khatsava <ilia-hatzava@mail.ru>
2023-06-03 22:03:20 +0300

    add scripts

--- script.js
+++ script.js
@@ -1,25 +1,94 @@
-let totalCount = 0;
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
 
-const setBtn = document.querySelector('.calc-button.double');
-const calcButton = document.querySelectorAll('.calc-button');
 const screen = document.querySelector('.screen');
 
-// function setTotal() {
-//     setBtn.addEventListener('click', () => {
-//         screen.innerHTML = totalCount;
-//     })
-// }
+function buttonClick(value) {
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
 
-calcButton.forEach(item => {
-    item.addEventListener('click', () => {
-        screen.innerHTML += item.innerHTML;
+function handleSymbol(symbol) {
+    switch(symbol) {
+        case "C":
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case "=":
+            if(previousOperator === null) {
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal;
+            break;
+        case '←':
+            if(buffer.length === 1){
+                buffer = '0';
+            }else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handleMath(symbol);
+            break;
+
+
+    }
+}
+
+
+function handleMath(symbol) {
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if(previousOperator === '+') {
+        runningTotal += intBuffer;
+    }else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }else if(previousOperator === '×'){
+        runningTotal *= intBuffer;
+    }else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+
+function handleNumber(numberString){
+    if(buffer === '0'){
+        buffer = numberString;
+    }else{
+        buffer += numberString;
+    }
+}
+
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
     })
-})
+}
 
-console.log(setBtn)
-
-setBtn.addEventListener('click', () => {
-    screen.innerHTML = totalCount;
-})
-
-
+init();
